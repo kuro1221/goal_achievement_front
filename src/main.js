b92093022f2d1cd3f30c3482fc4a810e278c85c4
kuro <toshras22@gmail.com>
2023-04-10 06:42:23 +0900

    カレンダー作成

--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-const app = createApp(App)
-
-registerPlugins(app)
-
-app.mount('#app')
+createApp(App).use(vuetify).mount('#app')
